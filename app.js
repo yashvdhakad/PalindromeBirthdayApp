c20f15b0f6336b96e53699b0011837b030480eac
Yash <yashvdkd@gmail.com>
2022-11-01 19:52:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const userDate = document.querySelector("#userDate");
+const checkBtn = document.querySelector("#checkBtn");
+const outputBox = document.querySelector("#outputBox");
+
+const checkBirthdayIsPalidromeOrNot = () => {
+    
+}
+
+checkBtn.addEventListener("click", checkBirthdayIsPalidromeOrNot);
